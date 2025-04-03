a421aa59924ce3b74c98ef0e0a923fbe78a0ded2
wikananda <nandasantana276@gmail.com>
2025-04-03 13:44:22 +0800

    ignore target features for multivariate custom dataset

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -225,10 +225,12 @@ class Dataset_Custom(Dataset):
         '''
         df_raw.columns: ['date', ...(other features), target feature]
         '''
-        cols = list(df_raw.columns)
-        cols.remove(self.target)
-        cols.remove('date')
-        df_raw = df_raw[['date'] + cols + [self.target]]
+        
+        if self.features == 'S':
+            cols = list(df_raw.columns)
+            cols.remove(self.target)
+            cols.remove('date')
+            df_raw = df_raw[['date'] + cols + [self.target]]
         num_train = int(len(df_raw) * 0.7)
         num_test = int(len(df_raw) * 0.2)
         num_vali = len(df_raw) - num_train - num_test
